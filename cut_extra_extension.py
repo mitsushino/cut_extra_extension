35005e3d54716a2949172220584c05a96e4c2c28
mitsushino <rkcckbk3@gmail.com>
2017-06-03 23:34:58 +0900

    ファイルを移動する処理を追加

--- cut_extra_extension.py
+++ cut_extra_extension.py
@@ -4,16 +4,24 @@
 
 import configparser
 import os
+import shutil
 import sys
 
 
 def cut_extra_extension(file_name):
+    '''
+    拡張子以降の余分な文字列を削除し、正規化したファイル名にリネームする
+    mp4.mp4mp4 -> mp4.mp4  mp4.mp4test.txt -> mp4.mp4  abcdef -> abcdef
+    :param file_name:
+    :return: 拡張子以降の余分な文字列を削除したファイル名
+    '''
     try:
         while True:
             period_position = file_name.rindex('.')
             file_name = file_name[:period_position + 4]
             ext = file_name[-3:]
-            if ext == 'mp4' or ext == 'vlc' or ext == 'exe':
+            if ext == 'mp4' or ext == 'vlc' or ext == 'exe' or ext == 'ini':
+                print('{}を変換しています・・・'.format(file_name))
                 return file_name
             else:
                 file_name = file_name[:-4]
@@ -32,14 +40,42 @@ def get_designated_path():
     return config['PASS']['copy_path']
 
 
+def move_file_to_designated_path(path, f):
+    '''
+    ファイルを指定されたパスに移動する
+    既にファイルが存在する場合は例外を出力して移動しない
+    exeファイルは移動しない
+    iniファイルは移動しない
+    '''
+    _, ext = os.path.splitext(f)
+    if ext == '.exe' or ext == '.ini':
+        return
+    move_path = os.path.join(path, f)
+    try:
+        if os.path.exists(move_path):
+            raise OSError
+        print('{}を{}へ移動します'.format(f, path))
+        return shutil.move(os.path.join(os.getcwd(), f),
+                           move_path)
+
+    except OSError:
+        print('{}が移動先フォルダ既に存在します。コピーしません。'.format(f))
+
+
 if __name__ == '__main__':
+    # 余分な拡張子の削除処理
     answer = input('ファイルの拡張子の余計な部分を削除しますか？[y/n]')
     if answer != 'y':
         sys.exit(0)
     current_folder_files = os.listdir('.')
     for f in current_folder_files:
-        print('{}を変換しています・・・'.format(f))
+        print('{}を変換します'.format(f))
         os.rename(f, cut_extra_extension(f))
+
+    # ファイルのコピー処理
+    current_folder_files = os.listdir('.')
     designated_path = get_designated_path()
+    for f in current_folder_files:
+        move_file_to_designated_path(designated_path, f)
 
     input('処理が完了しました')
--- cut_extra_extension_test.py
+++ cut_extra_extension_test.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+import os
+import shutil
 import unittest
+from send2trash import send2trash
 from cut_extra_extension import cut_extra_extension
 from cut_extra_extension import get_designated_path
+from cut_extra_extension import move_file_to_designated_path
 
 
 class CutExtensionTest(unittest.TestCase):
@@ -31,12 +35,44 @@ class CutExtensionTest(unittest.TestCase):
 
 
 class FileMoveTest(unittest.TestCase):
-    '''iniファイルを読み取り、ファイルのコピー先パスを取得するテスト'''
-    def test_copy_file_to_designated_path(self):
+    def test_get_designated_path(self):
+        '''iniファイルを読み取り、ファイルの移動先パスを取得するテスト'''
         config = configparser.ConfigParser()
         config.read('config.ini', encoding='utf-8')
         designated_path = config['PASS']['copy_path']
-        self.assertEqual(designated_path, get_designated_path())
+        self.assertEqual(get_designated_path(), designated_path)
+
+    def test_copy_file(self):
+        '''
+        ファイルを移動するテスト
+        '''
+        # テスト用の移動先フォルダ作成
+        folder_path = os.path.join(os.getcwd(), 'copy_dist')
+        if os.path.exists(folder_path):
+            send2trash(folder_path)
+        os.mkdir(folder_path)
+
+        # テスト用ファイル作成
+        test_file = 'mp4.mp4'
+        exception_test_file = 'mp42.mp4'
+        exe_file = 'test.exe'
+        ini_file = 'test.ini'
+        with open(test_file, mode='w', encoding='utf-8') as f: pass
+        with open(exception_test_file, mode='w', encoding='utf-8') as f: pass
+        shutil.move(exception_test_file, folder_path)
+        with open(exception_test_file, mode='w', encoding='utf-8') as f: pass
+        with open(exe_file, mode='w', encoding='utf-8') as f: pass
+        with open(ini_file, mode='w', encoding='utf-8') as f: pass
+
+        # テスト実行
+        self.assertEqual(os.path.join(os.getcwd(), 'copy_dist', test_file),
+                         move_file_to_designated_path(folder_path, test_file))
+        self.assertRaises(OSError,
+                          move_file_to_designated_path(folder_path, exception_test_file))
+        self.assertEqual(None,
+                         move_file_to_designated_path(folder_path, exe_file))
+        self.assertEqual(None,
+                         move_file_to_designated_path(folder_path, ini_file))
 
 
 if __name__ == '__main__':
